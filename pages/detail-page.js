eddcf7817468566ddbcfd1eaee7cdf741553d77f
“ALEX <alexgtr84@gmail.com>
2019-01-09 13:33:12 +0100

    form now records input

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,15 +1,29 @@
+// add event handler
 function submitComment() {
+    // gather data
     const inputField = document.getElementById('name')
     const name = inputField.value
     const textArea = document.getElementById('msg')
     const msg = textArea.value
+    
+    // create the elements you need
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
-    h3.innerHTML = '${name} said: '
+  
+    // adjust the elements we created
+    h3.innerHTML = `${name} said:`
     p.innerHTML = msg
-    comment.classlist.add('comment')
+    comment.classList.add('comment')
     comment.appendChild(h3)
     comment.appendChild(p)
-    console.log(comment)
+  
+    // display the elements on the page
+    const commentSection = document.getElementById('comments')
+    commentSection.appendChild(comment)
+
+    // reset the form
+    inputField.value = null
+    textArea.value = null
+
 }
